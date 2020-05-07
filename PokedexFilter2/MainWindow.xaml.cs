34eaec64f1389a982ceda21b007257c3c614fe0c
Gabriel <tetradog@gmail.com>
2020-05-07 17:15:48 +0200

    ahora es universal :D

---
+++ PokedexFilter2/MainWindow.xaml.cs
@@ -0,0 +1,255 @@
+﻿using Gabriel.Cat.S.Extension;
+using Gabriel.Cat.S.Utilitats;
+using PokemonGBAFramework.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PokedexFilter2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public static RomGba RomActual { get;  set; }
+        public static NombreTipo[] Tipos { get; set; }
+
+        Llista<Pokemon> filtroActual;
+        int posActual;
+        Thread hiloKeyDown;
+        private bool ejecutarCambio;
+
+        public MainWindow()
+        {
+
+            ContextMenu ctxMenu = new ContextMenu();
+            MenuItem item = new MenuItem();
+          
+            InitializeComponent();
+            posActual = 0;
+            item.Header = "Cambiar rom";
+            item.Click += (s, e) => PideRom();
+            ctxMenu.Items.Add(item);
+            ContextMenu = ctxMenu;
+            //pongo los pkm :D
+            PideRom();
+            cmbPokemons.SelectedIndex = 0;
+            gPokemonPrincipal.Espalda = false;
+        }
+
+        private void PideRom()
+        {
+            Label lblItemList;
+            Microsoft.Win32.OpenFileDialog opnRom = new Microsoft.Win32.OpenFileDialog();
+            SortedList<int, int> gruposHuevo = new SortedList<int, int>();
+            opnRom.Filter = "GBA|*.gba";
+            do
+            {
+                if (opnRom.ShowDialog().GetValueOrDefault())
+                {
+                    RomActual = new RomGba(opnRom.FileName);
+                    Tipos = NombreTipo.Get(RomActual);
+                    filtroActual = new Llista<Pokemon>(Pokemon.GetOrdenNacional(RomActual).Filtra((p)=>p.Descripcion.Altura!=default));
+                    //filtroActual.Sort();
+                    lstTipos.Items.Clear();
+                    for (int i = 0; i < Tipos.Length; i++)
+                    {
+                        lblItemList = new Label();
+                        lblItemList.Tag = i;
+                        lblItemList.Content = Tipos[i];
+                        lstTipos.Items.Add(lblItemList);
+                    }
+                    lstGruposHuevo.Items.Clear();
+                    for (int i = 0, f = Enum.GetNames(typeof(Stats.GrupoHuevo)).Length; i < f; i++)
+                    {
+                        lblItemList = new Label();
+                        lblItemList.Tag = i;
+                        lblItemList.Content = (Stats.GrupoHuevo)i;
+                        lstGruposHuevo.Items.Add(lblItemList);
+                        gruposHuevo.Add(i, i);
+                    }
+                    for (int i = 0; i < filtroActual.Count; i++)
+                    {
+                        if (!gruposHuevo.ContainsKey((int)filtroActual[i].Stats.GrupoHuevo1))
+                        {
+                            lblItemList = new Label();
+                            lblItemList.Tag = (int)filtroActual[i].Stats.GrupoHuevo1;
+                            lblItemList.Content = "#" + (int)filtroActual[i].Stats.GrupoHuevo1;
+                            lstGruposHuevo.Items.Add(lblItemList);
+                            gruposHuevo.Add((int)filtroActual[i].Stats.GrupoHuevo1, (int)filtroActual[i].Stats.GrupoHuevo1);
+                        }
+                        if (!gruposHuevo.ContainsKey((int)filtroActual[i].Stats.GrupoHuevo2))
+                        {
+                            lblItemList = new Label();
+                            lblItemList.Tag = (int)filtroActual[i].Stats.GrupoHuevo2;
+                            lblItemList.Content = "#" + (int)filtroActual[i].Stats.GrupoHuevo2;
+                            lstGruposHuevo.Items.Add(lblItemList);
+                            gruposHuevo.Add((int)filtroActual[i].Stats.GrupoHuevo2, (int)filtroActual[i].Stats.GrupoHuevo2);
+                        }
+                    }
+                    cmbPokemons.ItemsSource = filtroActual;
+                    PonPokemons();
+                }
+
+            } while (RomActual == null && MessageBox.Show("No se puede hacer nada sin rom...¿quieres volver a buscar?", "Se requiere ROM", MessageBoxButton.YesNo, MessageBoxImage.Exclamation) == MessageBoxResult.Yes);
+            if (RomActual == null)
+                this.Close();
+        }
+
+        private void PonPokemons()
+        {
+            gPokemonUp1.Pokemon = filtroActual.GetElementActual(Ordre.Consecutiu, posActual - 5);
+            gPokemonUp2.Pokemon = filtroActual.GetElementActual(Ordre.Consecutiu, posActual - 4);
+            gPokemonUp3.Pokemon = filtroActual.GetElementActual(Ordre.Consecutiu, posActual - 3);
+            gPokemonUp4.Pokemon = filtroActual.GetElementActual(Ordre.Consecutiu, posActual - 2);
+
+            gPokemonMedioUp.Pokemon = filtroActual.GetElementActual(Ordre.Consecutiu, posActual - 1);
+
+            gPokemonPrincipal.Pokemon = filtroActual[posActual];
+
+            gPokemonMedioDown.Pokemon = filtroActual.GetElementActual(Ordre.Consecutiu, posActual + 1);
+
+            gPokemonDown1.Pokemon = filtroActual.GetElementActual(Ordre.Consecutiu, posActual + 2);
+            gPokemonDown2.Pokemon = filtroActual.GetElementActual(Ordre.Consecutiu, posActual + 3);
+            gPokemonDown3.Pokemon = filtroActual.GetElementActual(Ordre.Consecutiu, posActual + 4);
+            gPokemonDown4.Pokemon = filtroActual.GetElementActual(Ordre.Consecutiu, posActual + 5);
+
+        }
+        //moverse :D
+        private void Grid_MouseWheel(object sender, MouseWheelEventArgs e)
+        {
+            bool isControlPress = Keyboard.IsKeyDown(Key.LeftCtrl) || Keyboard.IsKeyDown(Key.RightCtrl);
+            if (e.Delta < 0)
+            {
+                if (isControlPress)
+                    posActual += 9;
+                posActual++;
+            }
+            else
+            {
+                if (isControlPress)
+                    posActual -= 9;
+                posActual--;
+            }
+            if (posActual < 0)
+            {
+                posActual = filtroActual.Count - 1;
+            }
+            posActual = posActual % filtroActual.Count;
+            PonPokemons();
+        }
+
+        private void Window_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (hiloKeyDown == null)
+                if (e.Key == Key.Up || e.Key == Key.Down || e.Key == Key.Left || e.Key == Key.Right)
+                {
+                    if (hiloKeyDown != null)
+                    {
+                        hiloKeyDown.Abort();
+                        hiloKeyDown = null;
+                    }
+                    hiloKeyDown = new Thread(() => MueveListaConTeclado());
+                    hiloKeyDown.Start();
+                }
+        }
+        private void MueveListaConTeclado()
+        {
+            Action act = () => MueveListaConTecladoInvoked();
+            ejecutarCambio = true;
+            while (true)
+            {
+
+                Dispatcher.BeginInvoke(act);
+                Thread.Sleep(100);
+            }
+        }
+        private void MueveListaConTecladoInvoked()
+        {
+            bool isControlPress = Keyboard.IsKeyDown(Key.LeftCtrl) || Keyboard.IsKeyDown(Key.RightCtrl);
+            if (Keyboard.IsKeyDown(Key.Down) || Keyboard.IsKeyDown(Key.Right))
+            {
+                if (isControlPress)
+                    posActual += 9;
+                posActual++;
+            }
+            else
+            {
+                if (isControlPress)
+                    posActual -= 9;
+                posActual--;
+            }
+            if (posActual < 0)
+            {
+                posActual = filtroActual.Count + posActual;
+            }
+            posActual = posActual % filtroActual.Count;
+            if (ejecutarCambio)
+                PonPokemons();
+        }
+
+        private void Window_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Up || e.Key == Key.Down || e.Key == Key.Left || e.Key == Key.Right)
+            {
+                ejecutarCambio = false;
+                if (hiloKeyDown != null)
+                {
+                    hiloKeyDown.Abort();
+                    hiloKeyDown = null;
+                }
+            }
+        }
+
+        private void Filtra(object sender, RoutedEventArgs e)
+        {
+            SortedList<int, int> tipos = new SortedList<int, int>();
+            SortedList<int, int> gruposHuevo = new SortedList<int, int>();
+            int aux;
+            posActual = 0;
+            for (int i = 0; i < lstGruposHuevo.SelectedItems.Count; i++)
+            {
+                aux = (int)((Label)lstGruposHuevo.SelectedItems[i]).Tag;
+                gruposHuevo.Add(aux, aux);
+            }
+            for (int i = 0; i < lstTipos.SelectedItems.Count; i++)
+            {
+                aux = (int)((Label)lstTipos.SelectedItems[i]).Tag;
+                tipos.Add(aux, aux);
+            }
+            filtroActual = new Llista<Pokemon>(Pokemon.GetOrdenNacional(RomActual).Filtra((p) => p.Descripcion.Altura != default).Filtra((pokemon) => { return (gruposHuevo.Count == 0 || gruposHuevo.ContainsKey((int)pokemon.Stats.GrupoHuevo1) || gruposHuevo.ContainsKey((int)pokemon.Stats.GrupoHuevo2)) && (tipos.Count == 0 || tipos.ContainsKey((int)pokemon.Stats.Tipo1) || tipos.ContainsKey((int)pokemon.Stats.Tipo2)); }));
+            if (filtroActual.Count == 0)
+            {
+                filtroActual = new Llista<Pokemon>();
+                filtroActual.Add(Pokemon.Get(RomActual,0));
+
+            }
+            //filtroActual.Sort();
+            cmbPokemons.ItemsSource = filtroActual;
+            posActual = 0;
+            PonPokemons();
+        }
+
+        private void cmbPokemons_Selected(object sender, RoutedEventArgs e)
+        {
+            if (cmbPokemons.SelectedIndex >= 0)
+            {
+                posActual = cmbPokemons.SelectedIndex;
+                PonPokemons();
+            }
+        }
+    }
+}
---
+++ PokedexFilter2/PokemonViewer.xaml.cs
@@ -0,0 +1,204 @@
+﻿using Gabriel.Cat.S.Extension;
+using Gabriel.Cat.S.Utilitats;
+using PokemonGBAFramework.Core;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PokedexFilter2
+{
+    /// <summary>
+    /// Lógica de interacción para PokemonViewer.xaml
+    /// </summary>
+    public partial class PokemonViewer : UserControl
+    {
+        public static TwoKeysList<int, string, System.Windows.Media.Color> ColorTipo;
+
+        Pokemon pokemon;
+        bool espalda;
+        BitmapAnimated bmpImgAnimated;
+
+        private bool Animando { get; set; }
+
+        static PokemonViewer()
+        {
+            ColorTipo = new TwoKeysList<int,string, System.Windows.Media.Color>();
+
+            ColorTipo.Add(8,"ACERO", Colors.Gray);
+
+
+            ColorTipo.Add(11,"AGUA", Colors.Blue);
+
+
+            ColorTipo.Add(6,"BICHO", Colors.YellowGreen);
+
+
+            ColorTipo.Add(16,"DRAGÓN", Colors.MediumPurple);
+
+
+            ColorTipo.Add(13,"ELÉCT.", Colors.Gold);
+
+
+            ColorTipo.Add(7,"FANT.", Colors.Purple);
+
+
+            ColorTipo.Add(10,"FUEGO", Colors.OrangeRed);
+
+
+            ColorTipo.Add(15,"HIELO", Colors.AliceBlue);
+
+
+            ColorTipo.Add(1,"LUCHA", Colors.DarkRed);
+
+
+            ColorTipo.Add(0,"NORMAL", Colors.LightGray);
+
+            ColorTipo.Add(12,"PLANTA", Colors.LightGreen);
+
+
+            ColorTipo.Add(14,"PSÍQ.", Colors.LightPink);
+
+
+            ColorTipo.Add(5,"ROCA", Colors.SaddleBrown);
+
+
+            ColorTipo.Add(17,"SINIE.", Colors.Brown);
+
+
+            ColorTipo.Add(4,"TIERRA", Colors.SandyBrown);
+
+
+            ColorTipo.Add(3,"VENENO", Colors.Pink);
+
+            ColorTipo.Add(2,"VOLAD.", Colors.SteelBlue);
+
+            ColorTipo.Add(9,"(?)", Colors.White);
+
+        }
+        public PokemonViewer() : this(null) { }
+        public PokemonViewer(Pokemon pokemon, bool espalda = true)
+        {
+            Animando = false;
+            InitializeComponent();
+            if (pokemon != null)
+            {
+                this.Pokemon = pokemon;
+                this.Espalda = espalda;
+            }
+            else { this.espalda = true; }
+
+        }
+        public Pokemon Pokemon
+        {
+            get
+            {
+                return pokemon;
+            }
+
+            set
+            {
+                pokemon = value;
+                if (pokemon != null)
+                {
+                    txtNombre.Text = "#" + pokemon.OrdenNacional + " " + pokemon.Nombre;
+                    PonImagen();
+
+                    try
+                    {
+                        gsColor1.Color = ColorTipo.GetValueWithKey1((int)pokemon.Stats.Tipo1);
+                    }
+                    catch { gsColor1.Color = Colors.White; }
+                    try
+                    {
+                        gsColor2.Color = ColorTipo.GetValueWithKey1((int)pokemon.Stats.Tipo2);
+                    }
+                    catch { gsColor2.Color = Colors.Orange; }
+                }
+                if (bmpImgAnimated != null)
+                {
+                    if (Animando)
+                        bmpImgAnimated.Stop();
+                    bmpImgAnimated.FrameChanged -= PonImagenAnimacion;
+                }
+                if (pokemon != null)
+                    if (pokemon.Sprites != null)
+                    {
+                        bmpImgAnimated = pokemon.Sprites.Frontales.GetAnimacionImagenFrontal(pokemon.Sprites.PaletaNomal);
+                        bmpImgAnimated.FrameASaltarAnimacionCiclica = 1;
+                        bmpImgAnimated.AnimarCiclicamente = true;
+                        bmpImgAnimated.FrameChanged += PonImagenAnimacion;
+                        if (Animando)
+                            bmpImgAnimated.Start();
+                    }
+
+            }
+        }
+
+        private void PonImagen()
+        {
+            if (pokemon != null)
+            {
+                if (Espalda)
+                {
+                    img.SetImage((Bitmap)pokemon.Sprites.Traseros);
+                }
+                else
+                {
+                    img.SetImage((Bitmap)pokemon.Sprites.Frontales);
+                }
+            }
+        }
+
+        public bool Espalda
+        {
+            get
+            {
+                return espalda;
+            }
+
+            set
+            {
+                espalda = value;
+                PonImagen();
+            }
+        }
+
+        private void Grid_MouseEnter(object sender, MouseEventArgs e)
+        {
+            //se mueve :D
+            if (bmpImgAnimated != null)
+            {
+                bmpImgAnimated.Start();
+                Animando = true;
+            }
+
+        }
+
+        private void Grid_MouseLeave(object sender, MouseEventArgs e)
+        {
+            //deja de moverse
+            if (bmpImgAnimated != null)
+            {
+                bmpImgAnimated.Stop();
+                PonImagen();
+                Animando = false;
+            }
+        }
+
+        private void PonImagenAnimacion(BitmapAnimated bmpAnimated, Bitmap frameActual)
+        {
+            Action act = () => img.SetImage(frameActual);
+            Dispatcher.BeginInvoke(act);
+        }
+    }
+}
